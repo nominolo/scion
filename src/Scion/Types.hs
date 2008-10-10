4beed4284fa5da30443bd8118c79d8214f0f3df9
Thomas Schilling <nominolo@googlemail.com>
2008-10-10 17:55:31 +0100

    Use a simple IORef for Scion session state.
    This works much better with asynchronous exceptions than a CPS style monad
    and should avoid space leaks, too.

--- src/Scion/Types.hs
+++ src/Scion/Types.hs
@@ -1,41 +1,61 @@
 module Scion.Types where
 
-import GHC ( Ghc )
+import GHC
+import HscTypes
+import MonadUtils ( liftIO, MonadIO )
+import Exception
 import qualified GHC
+import Data.IORef
 
 data SessionState 
   = SessionState
 
+mkSessionState :: IO (IORef SessionState)
+mkSessionState = newIORef SessionState
+
 newtype ScionM a
-  = ScionM { 
-      unScionM :: forall r.
-                  SessionState
-               -> (SessionState -> a -> Ghc r)
-               -> Ghc r
-    }
+  = ScionM { unScionM :: IORef SessionState -> Ghc a }
 
 instance Monad ScionM where
-  return x = ScionM $ \s ka -> ka s x
+  return x = ScionM $ \_ -> return x
   (ScionM ma) >>= fb = 
-      ScionM $ \s kb ->
-          ma s $ \s' a ->
-          unScionM (fb a) s' kb             
+      ScionM $ \s -> do
+        a <- ma s 
+        unScionM (fb a) s             
 
 instance Functor ScionM where
   fmap f (ScionM ma) =
-      ScionM $ \s kb ->
-          ma s $ \s' a -> kb s' (f a)
+      ScionM $ \s -> fmap f (ma s)
+
+liftScionM :: Ghc a -> ScionM a
+liftScionM m = ScionM $ \_ -> m
+
+instance MonadIO ScionM where
+  liftIO m = liftScionM $ liftIO m
+
+instance ExceptionMonad ScionM where
+  gcatch (ScionM act) handler =
+      ScionM $ \s -> act s `gcatch` (\e -> unScionM (handler e) s)
+  gblock (ScionM act) = ScionM $ \s -> gblock (act s)
+  gunblock (ScionM act) = ScionM $ \s -> gunblock (act s)
+
+instance WarnLogMonad ScionM where
+  setWarnings = liftScionM . setWarnings
+  getWarnings = liftScionM getWarnings
+
+instance GhcMonad ScionM where
+  getSession = liftScionM getSession
+  setSession = liftScionM . setSession
 
 modifySessionState :: (SessionState -> SessionState) -> ScionM ()
 modifySessionState mod = 
-    ScionM $ \s k -> 
-        let s' = mod s in s' `seq` k s' ()
+    ScionM $ \r -> liftIO $ do s <- readIORef r; writeIORef r $! mod s
 
 getSessionState :: ScionM SessionState
-getSessionState = ScionM $ \s k -> k s s
+getSessionState = ScionM $ \s -> liftIO $ readIORef s
 
 gets :: (SessionState -> a) -> ScionM a
 gets sel = getSessionState >>= return . sel
 
 setSessionState :: SessionState -> ScionM ()
-setSessionState s' = ScionM $ \s k -> k s' ()
+setSessionState s' = ScionM $ \r -> liftIO $ writeIORef r s'
