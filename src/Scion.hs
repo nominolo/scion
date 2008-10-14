6eb2801132525f688b56b4f5a45ea3f958de70e6
Thomas Schilling <nominolo@googlemail.com>
2008-10-14 17:27:08 +0100

    More utils to retrieve settings from Cabal.

--- src/Scion.hs
+++ src/Scion.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PatternGuards #-}
 module Scion where
 
 import Scion.Types
@@ -5,10 +6,18 @@ import Scion.Types
 import GHC
 import GHC.Paths ( libdir )
 import MonadUtils ( liftIO, MonadIO )
+import Digraph          ( flattenSCCs )
 import System.Directory ( setCurrentDirectory )
+import SrcLoc
+
+import Control.Monad
+import Data.List        ( intercalate )
 
 import Distribution.Simple.LocalBuildInfo hiding ( libdir )
 import Distribution.Simple.Configure
+import Distribution.Simple.GHC ( ghcOptions )
+import Distribution.ModuleName ( components )
+import qualified Distribution.PackageDescription as PD
 
 --import Control.Exception
 
@@ -33,16 +42,62 @@ openCabalProject dist_dir = do
   lbi <- io $ getPersistBuildConfig dist_dir
   -- XXX: do something with old lbi before updating?
   modifySessionState $ \st -> st { localBuildInfo = Just lbi }
-{-
-loadCabalProject :: ScionM ()  -- XXX: return modules?
-loadCabalProject = do
-  lbi <- getLocalBuildInfo
-  dflags <- getSessionDynFlags
-  (dflags', unknown, warnings) <- parseDynamicFlags dflags lbi
--}
+
 getLocalBuildInfo :: ScionM LocalBuildInfo
 getLocalBuildInfo =
   gets localBuildInfo >>= \mb_lbi ->
   case mb_lbi of
     Nothing -> error "call openCabalProject before loadCabalProject"
     Just lbi -> return lbi
+
+data CabalComponent = Library | Executable String
+
+setDynFlagsFromCabal :: CabalComponent -> ScionM [PackageId]  -- XXX: return modules?
+setDynFlagsFromCabal component = do
+  lbi <- getLocalBuildInfo
+  dflags <- getSessionDynFlags
+  let pd = localPkgDescr lbi
+  bi <- case component of
+         Library
+          | Just lib <- PD.library pd -> return (PD.libBuildInfo lib)
+          | otherwise -> error "No library in package"
+         Executable n ->
+          case [ exe | exe <- PD.executables pd, PD.exeName exe == n ] of
+           [ exe ] -> return (PD.buildInfo exe)
+           [] -> error $ "Executable " ++ n ++ " not found in package"
+           _ -> error $ "Multiple executables found.  This is weird..."
+  let odir = buildDir lbi
+  let flags = ghcOptions lbi bi odir
+  (dflags', unknown, warnings) <- parseDynamicFlags dflags (map noLoc flags)
+  unless (null unknown) $
+    io $ putStrLn $ "Unrecognised flags:\n" ++ show (map unLoc unknown)
+  io $ mapM_ putStrLn $ map unLoc warnings
+  setSessionDynFlags dflags'
+
+setTargetsFromCabal :: CabalComponent -> ScionM ()
+setTargetsFromCabal Library = do
+  lbi <- getLocalBuildInfo
+  let lib = case PD.library (localPkgDescr lbi) of
+              Just l -> l
+              Nothing -> error "No library in package."
+  let modnames = PD.libModules (localPkgDescr lbi)
+  let cabal_mod_to_string m =
+        intercalate "." (components m)
+  let modname_to_target name =
+        Target { targetId = TargetModule (mkModuleName
+                                          (cabal_mod_to_string name))
+               , targetAllowObjCode = True
+               , targetContents = Nothing }
+  setTargets (map modname_to_target modnames)
+setTargetsFromCabal (Executable n) = do
+  error "unimplemented"
+
+-- in dep-order
+foldModSummaries :: GhcMonad m =>
+                    (a -> ModSummary -> m a) -> a
+                 -> m a
+foldModSummaries f seed = do
+  gr <- depanal [] False
+  let mss = flattenSCCs $ topSortModuleGraph False gr Nothing
+  foldM f seed mss
+
