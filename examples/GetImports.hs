a1904437e14944d88cac9250eadc3b8d615c3ebe
Thomas Schilling <nominolo@googlemail.com>
2008-10-14 17:27:49 +0100

    Update example program to do something more useful.

--- examples/GetImports.hs
+++ examples/GetImports.hs
@@ -1,9 +1,23 @@
 module Main where
 
+import GHC
 import Scion
 import MonadUtils ( liftIO )
+import qualified StringBuffer as SB
+import Outputable
+
+import Data.Maybe
 
 main = runScion $ do
-         liftIO $ print "hello"
-         setWorkingDir "./tests"
-         
+  liftIO $ print "hello"
+  setWorkingDir "../ghc/compiler"
+  openCabalProject "./dist-stage2"
+  setDynFlagsFromCabal Library
+  setTargetsFromCabal Library
+  load LoadAllTargets
+  cs <- flip foldModSummaries 0 $ \n ms -> do 
+    let n' = maybe 0 SB.len $ ms_hspp_buf ms
+    liftIO $ print (showSDoc (ppr (ms_mod_name ms)),
+                    n')
+    return $! n + n'
+  liftIO $ print ("total", cs)
