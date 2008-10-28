35500f97f0fd37ed83babee8173beb4f381f1fb3
Thomas Schilling <nominolo@googlemail.com>
2008-10-28 17:49:14 +0000

    Extend test case.

--- tests/Test001.hs
+++ tests/Test001.hs
@@ -1,9 +1,10 @@
+{-# OPTIONS_GHC -fglasgow-exts #-}
 module Main where
 
 import Data.Maybe ( fromMaybe )
 
 main = print (fromMaybe 0 (Just 3))
-
+{-
 data Foo = Foo0
          | Foo2 Foo Foo
 
@@ -14,3 +15,22 @@ instance Show Foo where
 f x ys =
     [ (x, y) | Just y <- ys
              , length y > 3 ]
+
+Just x = Just g
+
+g = x
+-}
+data T :: * -> * where
+  T :: Eq a => a -> T a
+
+x = undefined
+
+T i = T 33
+
+f (T i) = i == 42
+
+b = i == x
+
+r x y = case compare x y of
+          GT -> x + y
+          _  -> x - y
