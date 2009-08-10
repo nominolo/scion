f180be8ad625a2e0ff29323cc557054fc92f703b
Thomas Schilling <nominolo@googlemail.com>
2009-08-10 19:05:12 +0100

    Add `runScion'` to set static flags before starting the session.

--- lib/Scion.hs
+++ lib/Scion.hs
@@ -12,7 +12,7 @@
 module Scion 
   ( ScionM
   , liftIO, MonadIO
-  , module Scion  -- at least for now
+  , module Scion  -- re-export full module at least for now
   , module Scion.Session
   , module Scion.Utils
   , module Scion.Configure
@@ -24,7 +24,11 @@ import Scion.Configure
 import Scion.Utils
 
 import GHC
+import FastString ( fsLit )
+import Outputable ( printSDoc, defaultErrStyle, ppr, (<+>), text )
 import GHC.Paths ( libdir )
+
+import Control.Monad ( forM_ )
 
 -- | Run the 'ScionM' monad.
 runScion :: ScionM a -> IO a
@@ -34,3 +38,20 @@ runScion m = do
     r <- liftIO $ mkSessionState dflags
     setSessionDynFlags (initialScionDynFlags dflags)
     unScionM m r
+
+-- | Run the session with the given static flags.
+--
+-- Static flags cannot be changed during a session and can only be set once
+-- per /process/.  That is, any session running in the same process
+-- (i.e. program) must not attempt to set the static flags again.
+--
+-- Which flags are static flags depends on the version of GHC.
+runScion' :: [String] -> ScionM a -> IO a
+runScion' static_flags act = do
+  let fname = fsLit "<api-client>"
+      lflags = [ L (mkSrcSpan (mkSrcLoc fname line 0) (mkSrcLoc fname line (length s))) s
+                | (s,line) <- zip static_flags [1..] ]
+  (_leftovers, warnings) <- parseStaticFlags lflags
+  forM_ warnings $ \(L region msg) ->
+    printSDoc (ppr region <+> text msg) defaultErrStyle
+  runScion act
