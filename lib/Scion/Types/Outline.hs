344e22df2466fc5760bf3915cca9776e6ce236ba
Thomas Schilling <nominolo@googlemail.com>
2009-10-20 19:13:45 +0200

    Fix formatting in `S.Types.Outline`.

--- lib/Scion/Types/Outline.hs
+++ lib/Scion/Types/Outline.hs
@@ -10,28 +10,33 @@
 -- Outline representation of source code
 --
 module Scion.Types.Outline 
-( OutlineDef(..),
-  extractNames,trimLocationFile)
-  where
-
+  ( OutlineDef(..),
+    extractNames,trimLocationFile
+  )
+where
 
 import GHC
-
 import Scion.Types.Notes
 
-data OutlineDef=OutlineDef {
-	od_name::Either Name String,
-	od_type::String,
-	od_loc::Location,
-	od_block::Location,
-	od_parentName::Maybe (Name,String)
-	}
+import Data.List ( foldl' )
 
-	
+data OutlineDef = OutlineDef
+  { od_name       :: Either Name String,
+    od_type       :: String,
+    od_loc        :: Location,
+    od_block      :: Location,
+    od_parentName :: Maybe (Name,String)
+  }
+
 extractNames:: [OutlineDef] -> [Name]
-extractNames=foldl (\l od->case od_name od of
-	Left n->(n:l)
-	Right _->l) [] 
-	
+extractNames
+  = foldl' (\l od -> case od_name od of
+	               Left n -> n:l
+	               Right _ -> l)
+           []
+
 trimLocationFile:: [OutlineDef] -> [OutlineDef]
-trimLocationFile = map (\a@OutlineDef{od_loc=l,od_block=b}->a{od_loc=trimFile l,od_block=trimFile b})
+trimLocationFile =
+  map (\d@OutlineDef{ od_loc = l, od_block = b} -> 
+           d{ od_loc = trimFile l,
+              od_block = trimFile b})
